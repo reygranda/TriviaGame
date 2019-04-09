9b2860e3756eaad54481f5af2cf82180b54f2693
unknown <rey030299@gmail.com>
2019-04-09 15:25:40 -0400

    started js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,32 @@
+//var myQuestions = [
+//	question: "How many championships have the celtics won?"
+//	answers: {
+//		a: 10
+//		b: 11
+//		c: 17
+//		d: 15
+//	}
+//	question : "How many rings does Bill Russell have (alone as a player, not coach)?"
+//	answers: {
+//		a: 9
+//		b: 11
+//		c: 10
+//		d: 13
+//	}
+//	question:
+//	answers: {
+//		a: "Kobe Bryant, Shaquille O'Neal, Pau Gasol,  "
+//		b: ""
+//		c: ""
+//		d: ""
+//	}
+//];
+
+
 $(document).ready(function(){
-	$("#remaining-time").hide();
-	$("#start").on('click', trivia.startGame);
-    $(document).on('click' , '.option', trivia.guessChecker);
-
-})
-
-var trivia = {
-	
-	correct: 0,
-	incorrect: 0,
-	unanswered: 0,
-	currentSet: 0,
-	
-	
-	
-}
+	$("button").click(function(){
+		$("button").hide();
+		$(".display-4").replaceWith("<h1>How Many Championships have the celtics won?");
+		$(".lead").replaceWith("<h1>How Many Championships have the celtics won?");
+	});
+});
