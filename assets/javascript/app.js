d867d6e083d3d7d32db4e7a1f48924cb38ceb8b3
reygranda <rey030299@gmail.com>
2019-04-09 19:38:05 -0400

    laptop changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,5 @@
-//var myQuestions = [
-//	question: "How many championships have the celtics won?"
-//	answers: {
-//		a: 10
-//		b: 11
-//		c: 17
-//		d: 15
-//	}
-//	question : "How many rings does Bill Russell have (alone as a player, not coach)?"
-//	answers: {
-//		a: 9
-//		b: 11
-//		c: 10
-//		d: 13
-//	}
-//	question:
-//	answers: {
-//		a: "Kobe Bryant, Shaquille O'Neal, Pau Gasol,  "
-//		b: ""
-//		c: ""
-//		d: ""
-//	}
-//];
-
-
-$(document).ready(function(){
-	$("button").click(function(){
+$(document).ready(function () {
+	$("button").click(function () {
 		$("button").hide();
 		$(".display-4").replaceWith("<h1>How Many Championships have the celtics won?");
 		$(".lead").replaceWith("<h1>How Many Championships have the celtics won?");
