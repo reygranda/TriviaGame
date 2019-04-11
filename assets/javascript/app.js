f5996c7f1dd67fbcc9e818892f79b319538faa26
reygranda <rey030299@gmail.com>
2019-04-10 22:47:43 -0400

    class changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,18 @@
-$(document).ready(function () {
-	$("button").click(function () {
-		$("button").hide();
-		$(".display-4").replaceWith("<h1>How Many Championships have the celtics won?");
-		$(".lead").replaceWith("<h1>How Many Championships have the celtics won?");
-	});
-});
+var timer = 60;
+
+function timerFunction() {
+	setInterval(function() {
+	timer--;
+	if (timer === 0){
+		alert("Times Up!");
+		timer = 60;
+	}
+}, 1000);
+	
+setInterval(function() {
+	$("#timer").text(timer);
+}, 1000);
+}
+timerFunction();
+
+
